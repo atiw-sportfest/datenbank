8600f3507f11d93c60f62153e831050e51df6a18
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-06-27 01:28:29 +0200

    Remove data from dump (privacy)

--- sportfest_db.sql
+++ sportfest_db.sql
@@ -33,15 +33,6 @@ CREATE TABLE `anmeldung` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `anmeldung`
---
-
-LOCK TABLES `anmeldung` WRITE;
-/*!40000 ALTER TABLE `anmeldung` DISABLE KEYS */;
-/*!40000 ALTER TABLE `anmeldung` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `benutzer`
 --
 
@@ -57,16 +48,6 @@ CREATE TABLE `benutzer` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `benutzer`
---
-
-LOCK TABLES `benutzer` WRITE;
-/*!40000 ALTER TABLE `benutzer` DISABLE KEYS */;
-INSERT INTO `benutzer` VALUES ('admin','21232f297a57a5a743894a0e4a801fc3',1),('ewald','63f205e15d34aafe2b7a931bddfe467e',1),('richter1','63f205e15d34aafe2b7a931bddfe467e',2),('richter2','63f205e15d34aafe2b7a931bddfe467e',2),('root','63f205e15d34aafe2b7a931bddfe467e',1);
-/*!40000 ALTER TABLE `benutzer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `bewertungseinheit`
 --
 
@@ -79,16 +60,6 @@ CREATE TABLE `bewertungseinheit` (
   PRIMARY KEY (`BewertungseinheitID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bewertungseinheit`
---
-
-LOCK TABLES `bewertungseinheit` WRITE;
-/*!40000 ALTER TABLE `bewertungseinheit` DISABLE KEYS */;
-INSERT INTO `bewertungseinheit` VALUES (1,'cm'),(2,'Sek.'),(3,'Anz.');
-/*!40000 ALTER TABLE `bewertungseinheit` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `disziplin`
@@ -106,18 +77,8 @@ CREATE TABLE `disziplin` (
   `aktiviert` int(1) DEFAULT NULL,
   `teamleistung` int(1) DEFAULT NULL,
   PRIMARY KEY (`DisziplinID`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `disziplin`
---
-
-LOCK TABLES `disziplin` WRITE;
-/*!40000 ALTER TABLE `disziplin` DISABLE KEYS */;
-INSERT INTO `disziplin` VALUES (3,'2000m Lauf','Maximal  15  Starter  pro  Lauf. Die Damen starten in einem eigenen Lauf wenn mehr als 5 Starterinnen dabei sind.\n',3,3,1,1),(4,'4x100m Staffel','4  Läufer, Läuferinnen aus einer Klasse',4,4,1,1),(5,'Hochsprung','Es muss mit einem Bein abgesprungen werden ! Pro Höhe sind drei Versuche möglich. Es wird mindestens in 5 cm Schritten erhöht. Fällt die Latte in den ersten drei Sekunden nach dem Sprung nicht, gilt die Höhe als übersprungen.',3,3,1,0),(6,'Weitsprung','Sprung welcher so weit wi möglich sein sollte',5,5,1,0);
-/*!40000 ALTER TABLE `disziplin` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ergebnis`
@@ -142,16 +103,6 @@ CREATE TABLE `ergebnis` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ergebnis`
---
-
-LOCK TABLES `ergebnis` WRITE;
-/*!40000 ALTER TABLE `ergebnis` DISABLE KEYS */;
-INSERT INTO `ergebnis` VALUES (1,3,'sprung','weite des Sprungs','cm',1);
-/*!40000 ALTER TABLE `ergebnis` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `geschlecht`
 --
 
@@ -166,16 +117,6 @@ CREATE TABLE `geschlecht` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `geschlecht`
---
-
-LOCK TABLES `geschlecht` WRITE;
-/*!40000 ALTER TABLE `geschlecht` DISABLE KEYS */;
-INSERT INTO `geschlecht` VALUES (1,'männlich'),(2,'weiblich'),(3,'beides');
-/*!40000 ALTER TABLE `geschlecht` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `klasse`
 --
 
@@ -188,16 +129,6 @@ CREATE TABLE `klasse` (
   PRIMARY KEY (`KlassenID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `klasse`
---
-
-LOCK TABLES `klasse` WRITE;
-/*!40000 ALTER TABLE `klasse` DISABLE KEYS */;
-INSERT INTO `klasse` VALUES (1,'fs1501');
-/*!40000 ALTER TABLE `klasse` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `leistung`
@@ -224,15 +155,6 @@ CREATE TABLE `leistung` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `leistung`
---
-
-LOCK TABLES `leistung` WRITE;
-/*!40000 ALTER TABLE `leistung` DISABLE KEYS */;
-/*!40000 ALTER TABLE `leistung` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `leistungsergebnis`
 --
 
@@ -252,15 +174,6 @@ CREATE TABLE `leistungsergebnis` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `leistungsergebnis`
---
-
-LOCK TABLES `leistungsergebnis` WRITE;
-/*!40000 ALTER TABLE `leistungsergebnis` DISABLE KEYS */;
-/*!40000 ALTER TABLE `leistungsergebnis` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `regel`
 --
 
@@ -275,18 +188,8 @@ CREATE TABLE `regel` (
   `punkte` int(11) NOT NULL,
   PRIMARY KEY (`RegelID`),
   UNIQUE KEY `unique_idx` (`DisziplinID`,`idx`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `regel`
---
-
-LOCK TABLES `regel` WRITE;
-/*!40000 ALTER TABLE `regel` DISABLE KEYS */;
-INSERT INTO `regel` VALUES (1,'test',2,1),(2,'test3',3,1),(3,'test2',2,2),(4,'test1',2,3),(5,'test3',2,4);
-/*!40000 ALTER TABLE `regel` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `schüler`
@@ -310,16 +213,6 @@ CREATE TABLE `schüler` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `schüler`
---
-
-LOCK TABLES `schüler` WRITE;
-/*!40000 ALTER TABLE `schüler` DISABLE KEYS */;
-INSERT INTO `schüler` VALUES (2,'David','Pauli',1,1);
-/*!40000 ALTER TABLE `schüler` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `zustand`
 --
 
@@ -332,16 +225,6 @@ CREATE TABLE `zustand` (
   PRIMARY KEY (`ZustandsID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `zustand`
---
-
-LOCK TABLES `zustand` WRITE;
-/*!40000 ALTER TABLE `zustand` DISABLE KEYS */;
-INSERT INTO `zustand` VALUES (1,'Gewonnen'),(2,'Verloren'),(3,'Teilgenommen'),(4,'Unentschieden');
-/*!40000 ALTER TABLE `zustand` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Dumping routines for database 'sportfest'
