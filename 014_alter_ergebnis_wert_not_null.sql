12835f15d471dcf5ddcf6891de8cdb98236f6088
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-07-07 15:17:53 +0200

    Fix 014

--- 014_alter_ergebnis_wert_not_null.sql
+++ 014_alter_ergebnis_wert_not_null.sql
@@ -1,10 +1,18 @@
-ALTER TABLE `sportfest`.`ergebnis` 
-CHANGE COLUMN `Wert` `Wert` VARCHAR(256) NOT NULL ;
 
-CREATE DEFINER=`fs151`@`%` PROCEDURE `SchuelerAnzeigenMitLeistungAnDisziplin`(did int)
+ALTER TABLE `sportfest`.`ergebnis` CHANGE COLUMN `Wert` `Wert` VARCHAR(256) NOT NULL ;
+
+DROP PROCEDURE IF EXISTS SchuelerAnzeigenMitLeistungAnDisziplin;
+
+DELIMITER ;;
+
+CREATE PROCEDURE `SchuelerAnzeigenMitLeistungAnDisziplin`(
+    did int
+)
 BEGIN
 
-SELECT s.* FROM schueler s, leistung l
-WHERE l. disziplinID = did  AND s.SchuelerID = l.SchuelerID;
+    SELECT s.* FROM schueler s, leistung l
+    WHERE l. disziplinID = did  AND s.SchuelerID = l.SchuelerID;
 
-END
+END ;;
+
+DELIMITER ;
