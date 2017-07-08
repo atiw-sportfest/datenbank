d394f9613aaa38064b4f8cf932cb125c0e94dc42
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-07-08 15:13:57 +0200

    db_migration.sh

--- db_migration.sh
+++ db_migration.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-if [ -z "$MYSQL" ] ; then
-	read -p "MySQL Path: " -e MYSQL
+
+MYSQL="/usr/bin/mysql"
+MYSQL_USER="fs151"
+MYSQL_PASS="123QWEasd!"
+MYSQL_DB="sportfest"
+
+if [ ! -x "$MYSQL" ] && [ -z "$MYSQL" ] ; then
+    read -p "MySQL Path (Tab-Vervollständigung möglich)" -e MYSQL
 fi
-cat 0*.sql | "$MYSQL" -u fs151 -p"123QWEasd!" sportfest
+
+cat 0*.sql | "${MYSQL}" -u "$MYSQL_USER" -p"$MYSQL_PASS" ""$MYSQL_DB"
